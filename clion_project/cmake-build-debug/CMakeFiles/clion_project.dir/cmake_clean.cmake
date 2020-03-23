3cf226ed302c363483ddb73f04177e657d0724fe
peter A <MY_NAME@example.com>
2020-03-23 17:15:59 +0000

    amendment to some files

---
+++ clion_project/cmake-build-debug/CMakeFiles/clion_project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clion_project.dir/main.cpp.o"
+  "clion_project"
+  "clion_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/clion_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
