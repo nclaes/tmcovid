2278b0046efd705fdfc13bb29d1d114b1accef8e
Your Name <you@example.com>
2020-03-28 18:48:51 +0000

    update

---
+++ covid19_dataset_clion/cmake-build-debug/CMakeFiles/covid19_dataset_clion.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/covid19_dataset_clion.dir/main5.cpp.o"
+  "covid19_dataset_clion.exe"
+  "covid19_dataset_clion.pdb"
+  "libcovid19_dataset_clion.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/covid19_dataset_clion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
